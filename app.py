7033c035be61ed519be6b458d99363441f7701b8
Edelarosa86 <eddiedelarosa16@gmail.com>
2026-01-13 00:43:40 +0000

    project finished

---
+++ app.py
@@ -0,0 +1,15 @@
+estudiante = {
+    "nombre": "Eddie",
+    "edad": 39,
+    "calificacion_final": 4.5
+}
+print(estudiante)
+
+
+calificaciones = [3.3 , 6.5, 1.3, 7.8, 9.5]
+promedio = sum(calificaciones) / len(calificaciones)
+print(promedio)
+
+calificaciones = [3.3 , 6.5, 1.3, 7.8, 9.5]
+print(max(calificaciones))
+print(min(calificaciones))
